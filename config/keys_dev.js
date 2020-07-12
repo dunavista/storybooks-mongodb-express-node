1e966e0e2fb231792ea881e4ac54e6a85f3ba60c
dunavista <dunavista@gmail.com>
2020-07-12 10:08:36 +0100

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 PORT = xxxxx
 mongoURI = 'xxxxxxxxxxxxxxxxxxxxxx'
-GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxxx'
-GOOGLE_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+googleClientID = 'xxxxxxxxxxxxxxx'
+googleClientSecret = 'xxxxxxxxxxxxxxxxxxxxxxxx'
